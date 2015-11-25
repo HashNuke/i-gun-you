812ce4a7f4ec42a341edea331529986ce1961eb1
Akash Manohar J <akash@akash.im>
2015-11-25 09:02:14 +0530

    Pass a game into template

--- web/controllers/games_controller.ex
+++ web/controllers/games_controller.ex
@@ -2,7 +2,8 @@ defmodule IGunYou.GamesController do
   use IGunYou.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    game = %IGunYou.Game{}
+    render conn, "index.html", game: game
   end
 
   def show(conn, %{"slug" => game_slug} = params) do
